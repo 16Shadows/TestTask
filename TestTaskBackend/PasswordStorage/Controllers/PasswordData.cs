1fdfac5618f4020023c19224c82225a35d88224d
16Shadows <egor.0.00@mail.ru>
2024-07-10 13:57:12 +0500

    feat: Добавлен контроллер для работы с хранилищем паролей.
    Добавлен запрос GET api/passwords для получения списка паролей. Запрос имеет опциональный query-параметр filter. Если параметр указан, то будут возвращены строки, которые содержат подстроку, переданную как значение параметра. Если параметр не указан, то будут возвращены все пароли. Этот метод не возвращает значения паролей, а только их записи.
    
    Добавлен запрос GET api/passwords/{id} для получения записи пароля по конкретному ID. Этот метод не возвращает значение самого пароля.
    
    Добавлен запрос GET api/passwords/{id}/passwords для получения значения пароля конкретной записи.
    
    Добавлен запрос POST api/passwords для создания новой записи пароля. Метод принимает в теле запрос параметры password (значение пароля), passwordType (тип пароля), passwordFor (наименование сущности, для которой пароль создан).

---
+++ TestTaskBackend/PasswordStorage/Controllers/PasswordData.cs
@@ -0,0 +1,41 @@
+﻿using TestTaskBackend.PasswordStorage.Model;
+
+namespace TestTaskBackend.PasswordStorage.Controllers
+{
+	/// <summary>
+	/// Данные пароля, отправляемые клиенту. Не включают в себя сам пароль.
+	/// </summary>
+	public class PasswordData
+	{
+		/// <summary>
+		/// Идентификатор пароля
+		/// </summary>
+		public int Id { get; }
+		/// <summary>
+		/// Тип пароля. Одно из значений <see cref="PasswordEntryType"/> в виде строки.
+		/// </summary>
+		public string PasswordType { get; }
+		/// <summary>
+		/// Для чего пароль (наименование веб-сайта или email).
+		/// </summary>
+		public string PasswordFor { get; }
+		/// <summary>
+		/// Дата создания записи.
+		/// </summary>
+		public DateTime CreationTime { get; }
+
+		public PasswordData(int id, PasswordEntryType passwordType, string passwordFor, DateTime creationTime)
+		{
+			Id = id;
+			PasswordType = passwordType.ToString();
+			PasswordFor = passwordFor ?? throw new ArgumentNullException(nameof(passwordFor));
+			CreationTime = creationTime;
+		}
+
+		/// <summary>
+		/// Создаёт экземпляр <see cref="PasswordData"/> на основе данных <see cref="PasswordEntry"/>.
+		/// </summary>
+		/// <param name="password">Запись, на основе которой создаётся экземпляр.</param>
+		public PasswordData(PasswordEntry password) : this(password.Id, password.PasswordType, password.PasswordFor, password.CreationTime) { }
+	}
+}
